dd1a3b0834277c2d79d6fd23ca223c16391186c5
JavFuentes <javier.antonio8128@gmail.com>
2024-07-05 09:18:40 -0400

    Las imágenes se cargar con crossfade

--- composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/App.kt
+++ composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/App.kt
@@ -19,13 +19,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.AsyncImage
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        setSingletonImageLoaderFactory { context ->
+            ImageLoader.Builder(context)
+                .crossfade(true)
+                .logger(DebugLogger())
+                .build()
+        }
         Surface(modifier = Modifier.fillMaxSize()) {
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(120.dp),
