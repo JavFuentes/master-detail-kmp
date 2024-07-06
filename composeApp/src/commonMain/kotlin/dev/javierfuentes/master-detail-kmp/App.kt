b042751a9c0fd76b45e6bc057686d5d58d9aeaf4
JavFuentes <javier.antonio8128@gmail.com>
2024-07-06 04:58:31 -0400

    TopAppBar añadida dentro de un Scaffold

--- composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/App.kt
+++ composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/App.kt
@@ -1,7 +1,4 @@
-
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -11,9 +8,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,9 +25,13 @@ import coil3.compose.AsyncImage
 import coil3.compose.setSingletonImageLoaderFactory
 import coil3.request.crossfade
 import coil3.util.DebugLogger
+import master_detail_kmp.composeapp.generated.resources.Res
+import master_detail_kmp.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-@OptIn(ExperimentalCoilApi::class)
+
+@OptIn(ExperimentalCoilApi::class, ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
@@ -38,15 +42,27 @@ fun App() {
                 .logger(DebugLogger())
                 .build()
         }
+
         Surface(modifier = Modifier.fillMaxSize()) {
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(120.dp),
-                contentPadding = PaddingValues(4.dp),
-                horizontalArrangement = Arrangement.spacedBy(4.dp),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                items(movies, key = {it.id}){
-                    MovieItem(movie = it)
+
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(stringResource(Res.string.app_name)) }
+                    )
+                }
+
+            ) { padding ->
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(120.dp),
+                    contentPadding = PaddingValues(4.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                    modifier = Modifier.padding(padding)
+                ) {
+                    items(movies, key = {it.id}){
+                        MovieItem(movie = it)
+                    }
                 }
             }
         }
