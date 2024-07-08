626354cd3d16f96a1ae87c0f98a1ea6fa692eefe
JavFuentes <javier.antonio8128@gmail.com>
2024-07-08 11:15:22 -0400

    CircularProgressBar antes de cargar las imágenes

--- composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,16 @@
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -15,11 +18,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import master_detail_kmp.composeapp.generated.resources.Res
 import master_detail_kmp.composeapp.generated.resources.app_name
@@ -27,7 +32,10 @@ import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(onMovieClick: (Movie) -> Unit) {
+fun HomeScreen(
+        onMovieClick: (Movie) -> Unit,
+        vm: HomeViewModel = viewModel { HomeViewModel() }
+        ) {
     Screen {
         val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
         Scaffold(
@@ -40,6 +48,15 @@ fun HomeScreen(onMovieClick: (Movie) -> Unit) {
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
 
         ) { padding ->
+            val state = vm.state
+            if(state.loading) {
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(padding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
             LazyVerticalGrid(
                 columns = GridCells.Adaptive(120.dp),
                 contentPadding = PaddingValues(4.dp),
@@ -47,7 +64,7 @@ fun HomeScreen(onMovieClick: (Movie) -> Unit) {
                 verticalArrangement = Arrangement.spacedBy(4.dp),
                 modifier = Modifier.padding(padding)
             ) {
-                items(movies, key = { it.id }) {
+                items(state.movies, key = { it.id }) {
                     MovieItem(movie = it, onClick = { onMovieClick(it) })
                 }
             }
--- composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/ui/screens/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/javierfuentes/master-detail-kmp/ui/screens/home/HomeViewModel.kt
@@ -1,4 +1,27 @@
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 
 class HomeViewModel : ViewModel() {
+
+    var state by mutableStateOf(UiState())
+        private set
+
+    init {
+        viewModelScope.launch {
+            state = UiState(loading = true)
+            delay(1000)
+            state = UiState(loading = false, movies =  movies)
+        }
+    }
+
+    data class UiState(
+        val loading: Boolean = false,
+        val movies: List<Movie> = emptyList()
+    )
 }
